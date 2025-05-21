d0136ef3e960dc68a3eb8b7bcdbc165df6fa8f4f
Christofer Berruz Chungata <cberruzc@my.bridgeport.edu>
2025-05-21 11:07:45 -0700

    Exercises 1.5 and 1.6

--- ch1/lissajous/main.go
+++ ch1/lissajous/main.go
@@ -28,7 +28,12 @@ import (
 
 //!+main
 
-var palette = []color.Color{color.White, color.Black}
+// Exercise 1.5: Change palette from black on white to green on black
+var greenColor = color.RGBA{0x00, 0x80, 0x00, 0xff}
+var redColor = color.RGBA{0x80, 0x00, 0x00, 0xff}
+var blueColor = color.RGBA{0x00, 0x00, 0x80, 0xff}
+var yellowColor = color.RGBA{0xff, 0xff, 0x00, 0xff}
+var palette = []color.Color{color.Black, greenColor, redColor, blueColor, yellowColor}
 
 const (
 	whiteIndex = 0 // first color in palette
@@ -67,14 +72,19 @@ func lissajous(out io.Writer) {
 	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0 // phase difference
+	var colorIndex uint8
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
+			// Pick a color from the pallet. Exclude the 0th color (background), tho
+			colorIndex = uint8(rand.Intn(len(palette)-1) + 1)
+			// Exercise 1.6: modify the program to produce images of multiple color
+			// This can be done by picking a color at random.
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				blackIndex)
+				colorIndex)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
