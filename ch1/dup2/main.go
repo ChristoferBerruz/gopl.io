06b071238d2e974cfb8a44b0efbf22bd20742381
Christofer Berruz Chungata <cberruzc@my.bridgeport.edu>
2025-05-20 22:03:04 -0700

    Exercise 1.4: dup2 print names of all files where line exists

--- ch1/dup2/main.go
+++ ch1/dup2/main.go
@@ -16,9 +16,10 @@ import (
 
 func main() {
 	counts := make(map[string]int)
+	linesToFnames := make(map[string][]string)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, linesToFnames)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -26,21 +27,23 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, counts, linesToFnames)
 			f.Close()
 		}
 	}
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+			fmt.Printf("%d\t%s\t%v\n", n, line, linesToFnames[line])
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]int, linesToFnames map[string][]string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		counts[input.Text()]++
+		line := input.Text()
+		counts[line]++
+		linesToFnames[line] = append(linesToFnames[line], f.Name())
 	}
 	// NOTE: ignoring potential errors from input.Err()
 }
