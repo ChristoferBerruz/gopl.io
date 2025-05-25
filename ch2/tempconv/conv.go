61447f7e4df28563d31fb06a89c9253441c914f6
Christofer Berruz Chungata <cberruzc@my.bridgeport.edu>
2025-05-24 18:34:08 -0700

    Exercise 2.1: Adding Kelving converstions to tempconv package

--- ch2/tempconv/conv.go
+++ ch2/tempconv/conv.go
@@ -13,4 +13,12 @@ func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 // FToC converts a Fahrenheit temperature to Celsius.
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 
+func CToK(c Celsius) Kelvin { return Kelvin(c + Celsius(KelvinToCShift)) }
+
+func KtoC(k Kelvin) Celsius { return Celsius(k - Kelvin(KelvinToCShift)) }
+
+func FToK(f Fahrenheit) Kelvin { return CToK(FToC(f)) }
+
+func KtoF(k Kelvin) Fahrenheit { return CToF(KtoC(k)) }
+
 //!-
