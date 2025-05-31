f83584826726973d8f3932f781de02f66c632bae
Christofer Berruz Chungata <cberruzc@my.bridgeport.edu>
2025-05-30 21:58:33 -0700

    Saving surface into a file instead of showing it to console

--- ch3/surface/main.go
+++ ch3/surface/main.go
@@ -9,7 +9,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"math"
+	"os"
 )
 
 const (
@@ -24,20 +26,34 @@ const (
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
 
 func main() {
-	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+	filename := "surface.svg"
+	var out io.Writer
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0600)
+	if err != nil {
+		fmt.Printf("Error opening file %q: %v", filename, err)
+		fmt.Println("Defaulting to stdout")
+		out = os.Stdout
+	} else {
+		fmt.Printf("Successfully opened file %q. Output will be saved there.", filename)
+		out = file
+		// Defer closing the file before main returns
+		defer file.Close()
+	}
+	fmt.Fprintf(out, "<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
+
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
 			ax, ay := corner(i+1, j)
 			bx, by := corner(i, j)
 			cx, cy := corner(i, j+1)
 			dx, dy := corner(i+1, j+1)
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+			fmt.Fprintf(out, "<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
 				ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
-	fmt.Println("</svg>")
+	fmt.Fprintf(out, "</svg>")
 }
 
 func corner(i, j int) (float64, float64) {
